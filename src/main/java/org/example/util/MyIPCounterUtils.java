4aae12989ae6a2b3825dd0927a65f551f7317c05
AlexZink <zinkalexvvv@gmail.com>
2024-10-17 09:53:54 +0400

    Initial commit

---
+++ src/main/java/org/example/util/MyIPCounterUtils.java
@@ -0,0 +1,27 @@
+package org.example.util;
+
+import lombok.experimental.UtilityClass;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
+
+
+@UtilityClass
+public class MyIPCounterUtils {
+
+    private final Object PRESENT = new Object();
+
+    public int countUniqueIPs(Path filePath) throws IOException {
+
+        ConcurrentHashMap<String, Object> ipMap = new ConcurrentHashMap<>();
+
+        try (Stream<String> lines = Files.lines(filePath).parallel()) {
+            lines.forEach(line -> ipMap.putIfAbsent(line.trim(), PRESENT));
+        }
+
+        return ipMap.size();
+    }
+}
